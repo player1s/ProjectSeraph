ff40f66d96cf50d328f0d6b587be4f2598c0a90a
Levente <267589@via.dk>
2019-02-23 10:03:59 +0100

    Added: Model
    class: job

---
+++ model/Job.cs
@@ -0,0 +1,11 @@
+
+namespace ProjectSeraph.model
+{
+    class Job
+    {
+        public string Title { get; set; }
+        public string URL { get; set; }
+        public string time { get; set; }
+        public string salary { get; set; }
+    }
+}
